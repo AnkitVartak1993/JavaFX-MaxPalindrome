927e742e17db486011b57e602068b85664a5c820
AnkitVartak1993 <ankitvartak@gmail.com>
2017-06-01 00:46:33 -0400

    Rough template created and result label implemented

--- src/MaxPalindrome.java
+++ src/MaxPalindrome.java
@@ -1,9 +1,50 @@
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.geometry.Insets; 
+import javafx.geometry.Pos; 
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane; 
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+public class MaxPalindrome extends Application {
 
-public class MaxPalindrome {
-
+	@Override
+	public void start(Stage stage)
+	{
+		//creating labels and input fields
+		String maxPalFinal="Result : ";
+		Label result = new Label( maxPalFinal);
+		
+		
+		
+		GridPane gridPane = new GridPane();
+		gridPane.setMinSize(500, 500);
+	    gridPane.setPadding(new Insets(10, 10, 10, 10)); 
+	    gridPane.setVgap(5); 
+	    gridPane.setHgap(5);
+	    gridPane.setAlignment(Pos.CENTER);
+		
+	    gridPane.add(result,1,0);
+	    
+	    Scene scene = new Scene(gridPane); 
+	    stage.setTitle("Maximum Palindrome");
+	    
+	    stage.setScene(scene);
+	    stage.show(); 
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		launch(args);
 	}
 
 }
